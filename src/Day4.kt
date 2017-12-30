a72fd47a8896565f0598f70e8b1a5c855f8c386c
Ahsan Zaheer <ahsan.zaheer@clark.de>
2017-12-30 16:31:23 +0100

    Adding Day4 Files

---
+++ src/Day4.kt
@@ -0,0 +1,14 @@
+
+
+fun main(args: Array<String>) {
+    D4Task1()
+    D4Task2()
+}
+
+fun D4Task1() {
+//    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun D4Task2() {
+//    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
