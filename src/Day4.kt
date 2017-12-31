9e2d2abc1155eedf22d6724e381d2f4f99d276e6
Ahsan Zaheer <ahsan.zaheer@clark.de>
2017-12-31 02:53:36 +0100

    Day4 Task1 done

--- src/Day4.kt
+++ src/Day4.kt
@@ -1,4 +1,4 @@
-
+import java.io.File
 
 fun main(args: Array<String>) {
     D4Task1()
@@ -6,9 +6,26 @@ fun main(args: Array<String>) {
 }
 
 fun D4Task1() {
-//    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val lines = File("day4.txt").readLines()
+    val total = lines.fold(0, { acc: Int, l: String ->
+        val wordArray = getWordArray(l)
+        acc + (if(findWordInLine(wordArray)) 0 else 1)
+    })
+    println(total)
+}
+fun findWordInLine(words: List<String>): Boolean {
+    if (words.isEmpty()) return false
+    val word = words[0]
+    val res = words.drop(1).contains(word)
+    if (res) { return true }
+    return findWordInLine(words.drop(1).toList())
 }
 
 fun D4Task2() {
 //    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+fun getWordArray(line: String): List<String> {
+    return line.
+            split(Regex("\\s"))
 }
