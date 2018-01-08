54e238a03a73bf988edf69729c640c3d00ab95f1
Ahsan Zaheer <ahsan.zaheer@clark.de>
2018-01-08 03:24:53 +0100

    Task2 Day7

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,61 +1,77 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-//    val input = Day7Task1()
-//    println(input)
-    Day7Task2("tknk")
+    val input = Day7Task1()
+    println("Task1: $input")
+    Day7Task2(input)
 }
 
 fun Day7Task1(): String {
-    return findParent(getParent(lines.first()))
+    return findParent(getParentFromLine(lines.first()))
 }
 
 fun Day7Task2(input: String) {
-    println(findChildren(input))
-//    getRootParent
-//    getWeightOfParent
+    task2Process(input)
+}
+
+fun task2Process(input: String): Int {
+
     val line = findLineWithParent(input)
     val pWeight = getWeightFromLine(line)
-    val children = getChildrenList(line)
-    println ("wight $pWeight")
-    println ("children $children")
-    val listOfChildrenWeights
-//    getChildren
-//    getWeightsOfChildren (recursively)
+    val children = getChildrenListFromLine(line)
+    if (children.isEmpty()) return pWeight
+    val cWeights = children.map {
+        val v = task2Process(it)
+        if (v < 0) { return -1 }
+        Pair(v, it)
+    }.sortedBy { it.first }
+    val diff = cWeights.first().first - cWeights.last().first
+    if (diff != 0) {
+        var uniquePair: Pair<Int, String>? = null
+        val size = cWeights.size
+        if (cWeights.first().first != cWeights.get(1).first) { uniquePair = cWeights.first() }
+        else if (cWeights.last().first != cWeights.get(size - 2).first) { uniquePair = cWeights.last() }
+        uniquePair?.let {
+            val updatedWeight = getWeightOfParent(uniquePair!!.second) + diff
+            println("Task2:  $updatedWeight : Node: ${uniquePair.second}")
+            return -1
+        }
+    }
+    return cWeights.fold(pWeight, {
+        acc, i -> acc + i.first
+    })
 }
 
 fun findLineWithParent(parent:String): String {
-//
-
-    val line = lines.filter { it.contains(Regex("($parent).*(\\d)"))}
-    if (line.isEmpty()) return ""
-    return line.first()
-}
-fun findLineWithChild(child:String) {
-
+    val filteredList = lines.filter { it.contains(Regex("($parent).*(\\d)"))}
+    return if (filteredList.isNotEmpty()) filteredList.first() else ""
 }
 
 fun findParent(child: String): String {
     val line = lines.filter { it.contains(Regex("(->).*($child)"))}
     if (line.isEmpty()) return child
-    return findParent(getParent(line.first()))
+    return findParent(getParentFromLine(line.first()))
 }
 
-fun findChildren(parent: String): List<String> {
-    val list = lines.filter { it.contains(Regex("($parent).*(->)")) }
-    return getChildrenList(list.first())
+fun getChildrenListFromLine(line: String): List<String> {
+    val ret = line.replace(Regex("\\s+"), "").split(Regex("->"))
+    if (ret.size < 2) return emptyList()
+    ret.get(1).let { it ->
+        return it.split(",")
+    }
 }
 
-fun getChildrenList(line: String): List<String> {
-    return line.split(Regex("\\s->\\s")).getOrNull(1)?.split(",")?: emptyList()  // .replace(Regex("\\s"), "").split(",")
-}
-
-fun getParent(line: String): String {
+fun getParentFromLine(line: String): String {
     return line.split(Regex("\\s")).first()
 }
 
-fun getWeightOfParent(parent:String) {
+fun getWeightOfParent(parent:String): Int {
+    return getWeightFromLine(findLineWithParent(parent))
+}
 
+fun isRootNode(node:String): Boolean {
+    val line = lines.filter { it.contains(Regex("(->).*($node)"))}
+    return line.isEmpty()
 }
 
 fun getWeightFromLine(line: String): Int {
@@ -63,13 +79,7 @@ fun getWeightFromLine(line: String): Int {
 }
 
 val lines :List<String> = getTaskInput()
-    get() {
-        if (field == null) {
-            field = getTaskInput()
-        }
-        return field
 
-    }
 
 fun getTaskInput(): List<String> {
     return File("day7.txt").readLines()
